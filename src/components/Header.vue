8e855281d51d89546ad06fda5b48b1d6c436345d
RawZ06 <longordo.alexandre@gmail.com>
2020-02-26 23:13:51 +0100

    Add link to home page on logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
 		<div class="title-version">
-			<img src="../img/logo.png" width="200px" alt="" class="title">
+			<a href="/"><img src="../img/logo.png" width="200px" alt="" class="title"></a>
 			<small>v.3.0.3 <span class="badge badge-success">Release</span></small>
 			<div title="Menu" class="c-icon c-icon-burger"></div>
 		</div>
