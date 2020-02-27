04b8fc2093746a24a5a3226f9975615a53692054
RawZ06 <longordo.alexandre@gmail.com>
2020-02-28 00:06:31 +0100

    Version 3.0.6

--- src/components/Changelog.vue
+++ src/components/Changelog.vue
@@ -7,6 +7,10 @@
 				<md-tab id="beta-3-version" md-label="V3 BETA"/>
 			</md-tabs>
 				<div v-if="tab === 'classic-version'">
+					<h3>VERSION 3.0.6<span class="badge badge-danger">New</span></h3>
+					<ul>
+						Nothing just for test CI
+					</ul>
 					<h3>VERSION 3.0.5<span class="badge badge-danger">New</span></h3>
 					<ul>
 						<li>Adding elements for SEO</li>
